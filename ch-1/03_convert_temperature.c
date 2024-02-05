19c52f7477f7782a0a3374305a7e3f9a545b09f6
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-04 20:13:00 -0500

    coonvert temperature

--- ch-1/03_convert_temperature.c
+++ ch-1/03_convert_temperature.c
@@ -3,6 +3,10 @@
 int main(int argc, char const *argv[])
 {
    float celcius;
+   printf("Celcius: ");
+   scanf("%f", &celcius);
+
+   printf("Farenheit: %.2f", ((celcius * 9/5) + 32));
    
    return 0;
 }
