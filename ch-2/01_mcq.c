a2e241f2c545488c77d623852d0aa9908144ded2
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-04 20:14:05 -0500

    quick quiz

---
+++ ch-2/01_mcq.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   int v = 3^3;
+   char dt = "21 dwev";// incorrect answer because char can store in single char
+   printf("%c",dt);
+   return 0;
+}
---
+++ ch-3/01_output_check.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   // output of program will be 11
+   int a = 10;
+   if (a = 11)
+   {
+      printf("I am 11");
+   } 
+   else {
+      printf("I am not 11");
+   }
+   return 0;
+}
