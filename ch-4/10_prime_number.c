22745163286f069dad6e9120b2dce433fa822d34
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-05 19:44:41 -0500

    find prime number

---
+++ ch-4/10_prime_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   int num = 3, prime = 1; // Assume it's prime until proven otherwise
+
+   for (int i = 2; i < num; i++)
+   {
+      if (num % i == 0)
+      {
+         prime = 0; // Set prime to 0 if a factor is found
+         break;     // No need to continue checking
+      }
+   }
+
+   if (num <= 1)
+      prime = 0; // 1 is not considered a prime number
+
+   if (prime == 1 || prime == 2)
+   {
+      printf("%d is prime\n", num);
+   }
+   else
+   {
+      printf("%d is not prime\n", num);
+   }
+
+   return 0;
+}
