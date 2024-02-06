fdfc8038ef477d4cc8b1fe52843d5fb07b529968
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-05 19:39:03 -0500

    multiplication

---
+++ ch-4/01_multiplication_table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+   int num;
+   printf("Enter the number: ");
+   scanf("%d",&num);
+
+   for (int i = 0; i <= 10; i++)
+   {
+      printf("%d * %d = %d\n", num, i, (num * i));
+   }
+
+   
+   return 0;
+}
