aea4c73bbe3c5f64d394b97c29482f395c8784b2
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-05 19:42:46 -0500

    question

---
+++ ch-2/04_precdence_associativty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   int result = 3*2/3-3+1;
+   printf("%d",result);
+   // 6/3-3+1
+   // 2-3+1
+   // -1+1
+   // 0
+   return 0;
+}
