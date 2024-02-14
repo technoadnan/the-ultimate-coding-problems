19bb0c95a4ff57da37def6d19d9cd83b039901e4
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-13 22:55:13 -0500

    multplication table 3x10

---
+++ ch-7-arrays/07_multiple_multiplication_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void multiplication_table(int multarr[], int num)
+{
+   for (int i = 1; i <= 10; i++)
+   {
+      multarr[i] = i * num;
+   }
+
+   for (int i = 1; i <= 10; i++)
+   {
+      printf("%d x %d = %d\n", num, i, multarr[i]);
+   }
+   
+   
+}
+
+int main(int argc, char const *argv[]) {
+   int arr[3][10];
+   multiplication_table(arr[0], 2);
+   printf("\n");
+   multiplication_table(arr[0], 7);
+   printf("\n");
+   multiplication_table(arr[0], 9);
+   printf("\n");
+
+   
+   return 0;
+}
