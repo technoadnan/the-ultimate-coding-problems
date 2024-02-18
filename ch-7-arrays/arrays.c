c3066586e3790d94476c4d207ac7be587138d3bb
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-17 21:43:24 -0500

    arrays

--- ch-7-arrays/arrays.c
+++ ch-7-arrays/arrays.c
@@ -19,8 +19,6 @@ void printArray(int ptr[], int n)
    
 }
 
-
-
 int main(int argc, char const *argv[]) {
    int marks[4];
    int *ptr;
@@ -38,7 +36,6 @@ int main(int argc, char const *argv[]) {
    for (int i = 0; i < 4; i++)
    {
       printf("The value of marks for students %d is %d \n",i+1, marks[i]);
-
    }
    return 0;
 }
