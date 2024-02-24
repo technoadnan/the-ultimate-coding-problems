6357b7bcc05f4f3bfbf0a2f1ec630abba3943dcc
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-23 20:26:47 -0500

    realloc initial

---
+++ ch-11-dynamic-memory-allocation/04_realloc_store.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]) {
+   int *ptr;
+   ptr = (int*) malloc(5 * sizeof(int));
+   for (int i = 0; i < 5; i++)
+   {
+      int num;
+      scanf("%d",&num);
+      ptr[i] = num;
+   }
+
+   for (int i = 0; i < 5; i++)
+   {
+      printf("%d\n", ptr[i]);
+   }
+
+
+   ptr = realloc(ptr, 10*sizeof(int));
+   for (int i = 0; i < 10; i++)
+   {
+      scanf("%d", &ptr[i]);
+   }
+   for (int i = 0; i < 10; i++)
+   {
+      printf("%d", ptr[i]);
+   }
+   
+
+
+   return 0;
+}
