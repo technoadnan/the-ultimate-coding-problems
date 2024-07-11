fdf4444c69e2c3da0cd596c3379e7b37f721fc1a
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-07-10 23:32:09 -0500

    Refactor nested loop logic in 08_nested_loop.py

--- py-problems/hackerrank/easy/08_nested_loop.py
+++ py-problems/hackerrank/easy/08_nested_loop.py
@@ -2,32 +2,16 @@ import time
 if __name__ == '__main__':
    # store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
    
-   py_students = [['ha', 37.21], ['r', 37.21],['wrr', 37.2],['ertr', 41],['fdger', 39]]
-   # for _ in range(int(input())):
-   #    name = input()
-   #    score = float(input())
-   #    py_students.append([name,score])
+   py_students = []
+   # py_students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+   for _ in range(int(input())):
+      name = input()
+      score = float(input())
+      py_students.append([name,score])
 
-   # for j in py_students:
-   lowest_score = float('inf') # to find lowest number
-   # second_lowest = 
-   # for i, k in enumerate(py_students):
-   #    if k[1] < lowest_score:
-   # #       lowest_score = k[1]
-   #       print(k)
-   # print(lowest_score)
+   all_scores = sorted(set([score for name,score in py_students]))
+   second_lowest_num = all_scores[1]
 
-   # for j in py_students:
-   #    print(j)
+   second_lowest_students = sorted([name for name,score in py_students if score == second_lowest_num])
 
-   all_scores = set()
-   for j in range(len(py_students)):
-      all_scores.add(py_students[j][1])
-   print(sorted(all_scores)[1])
-
-      # if py_students[j][1] < lowest_score:
-      #    lowest_score = py_students[j][1]
-      # print(py_students[j][1])
-      # print(j)
-
-   # print(lowest_score)
+   for name in second_lowest_students:print(name)
