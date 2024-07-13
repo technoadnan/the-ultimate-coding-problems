022da240d3a4a6d20931084f36abc894d03b562b
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-07-13 10:29:35 -0500

    Refactor list operations logic in 10_lists.py

--- py-problems/hackerrank/easy/10_lists.py
+++ py-problems/hackerrank/easy/10_lists.py
@@ -4,21 +4,24 @@ if __name__ == '__main__':
    for _ in range(N):
       command = input("")
       multiple_cmd = command.split(" ")
-      # print(command)
-      c1 = int(multiple_cmd[1])
-      c2 = int(multiple_cmd[2])
-      print(l)
-      if command[0] == "insert":
+
+      if len(multiple_cmd) > 2:
+         c1 = int(multiple_cmd[1])
+         c2 = int(multiple_cmd[2])
+      elif len(multiple_cmd) > 1:
+         c1 = int(multiple_cmd[1])
+      
+      if multiple_cmd[0] == "insert":
          l.insert(c1, c2)
-      elif command == "print":
+      elif multiple_cmd[0] == "print":
          print(l)
-      elif command == "remove":
+      elif multiple_cmd[0] == "remove":
          l.remove(c1)
-      elif command == "append":
+      elif multiple_cmd[0] == "append":
          l.append(c1)
-      elif command == "sort":
+      elif multiple_cmd[0] == "sort":
          l.sort()
-      elif command == "pop":
+      elif multiple_cmd[0] == "pop":
          l.pop()
-      elif command == "reverse":
+      elif multiple_cmd[0] == "reverse":
          l.reverse()
