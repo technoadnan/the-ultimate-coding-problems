773eb718310814f0de746ed0b3e150c9978c82aa
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-07-16 20:17:35 -0500

    Refactored with order

--- py-problems/hackerrank/medium/02_merge_the_tools.py
+++ py-problems/hackerrank/medium/02_merge_the_tools.py
@@ -1,17 +1,16 @@
-from time import sleep
-
 def merge_the_tools(string, k):
    all_sub_str = []
    # your code goes here
    new_str = ""
    for j in range(len(string)):
       new_str += string[j]
-
       if (j + 1) % k == 0:
-         all_sub_str.append(set(new_str)) 
+         unique_char = ''.join(dict.fromkeys(new_str))
+         all_sub_str.append(unique_char)
          new_str = ""
+
    for j in all_sub_str:
-      print(''.join(j))
+      print(j)
 
 if __name__ == '__main__':
    string, k = input(), int(input())
