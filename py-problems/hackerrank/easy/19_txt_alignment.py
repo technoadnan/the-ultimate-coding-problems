5c8ba067b3239f985ac32e9a01648a95d2a09ebe
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-08-05 23:54:42 -0400

    Fix formatting in 19_txt_alignment.py

--- py-problems/hackerrank/easy/19_txt_alignment.py
+++ py-problems/hackerrank/easy/19_txt_alignment.py
@@ -1,7 +1,7 @@
 thickness = int(input()) # This must be an odd number
 c = 'H'
 
-# Top Cone
+# Top Cone 
 for i in range(thickness):
    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
