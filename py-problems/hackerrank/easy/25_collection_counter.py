51eb2032234dd71da118932c6b90eb576d1fd33a
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-08-13 21:20:27 -0400

    Add solution for HackerRank problem: "Implement Collection Counter"

---
+++ py-problems/hackerrank/easy/25_collection_counter.py
@@ -0,0 +1,15 @@
+num_of_shoe = int(input(""))
+
+l = [int(x) for x in input("").split()]
+print(l)
+
+num_of_customer = int(input(""))
+
+profit = 0
+for i in range(num_of_customer):
+   customer_shoe_size,price = map(int, input("").split())
+   if customer_shoe_size in l:
+      l.remove(customer_shoe_size)
+      profit += price
+
+print(profit)
