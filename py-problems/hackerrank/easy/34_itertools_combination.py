7ef5370635da194fcd8331341243c5d85cca072c
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-08-23 23:50:48 -0400

    Refactor code in 34_itertools_combination.py

--- py-problems/hackerrank/easy/34_itertools_combination.py
+++ py-problems/hackerrank/easy/34_itertools_combination.py
@@ -2,6 +2,7 @@ from itertools import combinations_with_replacement
 
 string, number = map(str, input("").split())
 all_permutations = list(sorted(combinations_with_replacement(string, int(number))))
+
 for j in all_permutations:
    str_format = ''.join(j)
    print(str_format)
