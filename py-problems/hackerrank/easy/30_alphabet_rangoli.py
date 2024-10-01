7d273abd117b595727631c7458d3a732d593d831
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-09-30 22:41:36 -0400

    Refactor print_rangoli function in 30_alphabet_rangoli.py to improve code readability and performance

--- py-problems/hackerrank/easy/30_alphabet_rangoli.py
+++ py-problems/hackerrank/easy/30_alphabet_rangoli.py
@@ -1,7 +1,13 @@
+
 def print_rangoli(size):
-   # your code goes here
-   pass
+    import string
+    letters = string.ascii_lowercase[:size]
+    lines = []
+    for i in range(size):
+        s = '-'.join(letters[size-1:i:-1] + letters[i:size])
+        lines.append(s.center(4*size - 3, '-'))
+    print('\n'.join(lines[::-1] + lines[1:]))
 
 if __name__ == '__main__':
-   n = int(input())
-   print_rangoli(n)
+    n = int(input())
+    print_rangoli(n)
