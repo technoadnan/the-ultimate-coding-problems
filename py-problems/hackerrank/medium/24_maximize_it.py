340fccf5a942ff278f94b89e58911f23d8e99f29
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-10-08 23:39:20 -0400

    Add solution for HackerRank problem: "Implement maximum sum of squares calculation in Python"

---
+++ py-problems/hackerrank/medium/24_maximize_it.py
@@ -0,0 +1,20 @@
+from itertools import product
+
+# Function to generate combinations using *args
+def generate_combinations(*args):
+   return product(*args)
+
+num_lines = input("").split(" ")
+
+lines = []
+
+for _ in range(int(num_lines[0])):
+   l = list(map(int,input().split()))[1:]
+   lines.append(l)
+combinations = product(*lines)
+
+max_num = []
+for comb in combinations:
+   sq = (*map(lambda x:x**2, comb),) # explain
+   max_num.append(sum(sq)%int(num_lines[1]))
+print(max(max_num))
