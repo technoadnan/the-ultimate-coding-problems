7aef768acf5c522dbd905448be90e7c45a5a9e88
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-04 23:01:40 -0500

    Refactor sumDouble class in Java to use ternary operator

---
+++ java/codingBat/warmup-1/sumDouble.java
@@ -0,0 +1,17 @@
+// public class sumDouble {
+//    public int sumDouble(int a, int b) {
+//       // if (a == b){
+//       //   return (a+b) * 2;
+//       // }
+//       // return a + b;
+      
+//       return (a == b) ? (a+b) * 2 : a + b;
+//     }
+    
+// }
+
+public class sumDouble {
+    public int sumDouble(int a, int b) {
+        return (a == b) ? (a + b) * 2 : a + b;
+    }
+}
