f49eb7b929d1e0236c1867e95268418d9bfd8778
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-11-04 23:01:45 -0500

    Refactor diff21 class in Java to use conditional operator

---
+++ java/codingBat/warmup-1/diff21.java
@@ -0,0 +1,12 @@
+public class diff21 {
+   public int diff21(int n) {
+      int sub = 21 - n;
+      if (sub < 0) { // if sub is negative; converting to positive
+        sub = sub * -1;
+      }
+      return (n > 21) ? sub *2 : sub;
+      
+      
+    }
+    
+}
