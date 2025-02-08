a7bc9abc76055a87e477fb1d3030661b8b615a57
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-02-07 23:58:42 -0500

    Refactor notString method in Java to remove commented out code

--- java/codingBat/warmup-1/09_notString/notString.java
+++ java/codingBat/warmup-1/09_notString/notString.java
@@ -5,7 +5,7 @@ public String notString(String str) {
     if((str.length() >= 3 && str.substring(0, 3).equals("not"))){
         return str;
     }
-    // if(notSubString.equals("not") && str.length() >= 3){
+    // if(notSubString.equals("not") && str.length() >= 3){ 
     //   return str;
     // }
     else{
