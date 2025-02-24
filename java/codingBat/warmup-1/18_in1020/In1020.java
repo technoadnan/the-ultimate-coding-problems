3ec474ce084242d4d10829bc9640c78cc3611c62
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-02-23 23:58:04 -0500

    Add solution for Codingbat problem: "Implement In1020 class in Java"

---
+++ java/codingBat/warmup-1/18_in1020/In1020.java
@@ -0,0 +1,11 @@
+public class In1020 {
+    public static boolean in1020(int a, int b) {
+        // inequality
+        // if((10 >= a >= 20 )|| (10 >= b  >= 20)){
+          return ((a >= 10 && a <= 20) || (b>=10 && b <= 20));
+      }
+    public static void main(String[] args) {
+        // Your code here
+        in1020(4,3);
+    }
+}
