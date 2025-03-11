ea98de09ebc5f82a3905c8de5d466f4d53d90287
Adnan Bin Alam <code.technoadnan@gmail.com>
2025-03-10 23:59:15 -0400

    Refactor parrotTrouble method in Java codingBat/warmup-1/05_parrotTrouble/parrotTrouble.java

---
+++ java/codingBat/warmup-1/05_parrotTrouble/parrotTrouble.java
@@ -0,0 +1,8 @@
+public boolean parrotTrouble(boolean talking, int hour) {
+    // if  (7 > hour > 20) && (talking == true) {
+    // if ((hour<7 || hour > 20) && talking == true){
+    //   return true;
+    // }
+    // return false;
+    return ((hour<7 || hour > 20) && talking == true);
+}
